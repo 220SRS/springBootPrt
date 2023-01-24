cd16bda0ce17b56a7cca86e9e9a68a5ee40a5316
220SRS <jesikrjy@gmail.com>
2023-01-25 02:08:12 +0900

    웹페이지에서 id 번호를 입력하여 데이터 조회하기

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능!
 @AllArgsConstructor
+@NoArgsConstructor //디폴트 생성자를 추가해주는 어노테이션
 @ToString
 public class Article {
     @Id // 대표값을 지정! like a 주민번호
@@ -19,4 +21,5 @@ public class Article {
     private String title;
     @Column
     private String content;
+
 }
