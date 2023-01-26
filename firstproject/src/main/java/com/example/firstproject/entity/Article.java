cd9ce12600ff143533c4fa9e87529bf8b7b99c4e
220SRS <jesikrjy@gmail.com>
2023-01-26 19:07:42 +0900

    log 창에서 sql 보는 것과 형식 지정 및 id번호 지정을 직접 정하는게 아닌 DB가 정하는 방식으로 코드 수정

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,19 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 인식 가능!
+@Entity // DB가 해당 객체를 인식 가능! (해당 클래스로 테이블을 만든다는 말)
 @AllArgsConstructor
 @NoArgsConstructor //디폴트 생성자를 추가해주는 어노테이션
 @ToString
 @Getter
 public class Article {
     @Id // 대표값을 지정! like a 주민번호
-    @GeneratedValue // 1, 2, 3, ... 자동 생성 어노테이션
+//    @GeneratedValue // 1, 2, 3, ... 자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 중복을 막기 위해 DB가 ID를 자동 생성
     private Long id;
     @Column //DB 안에서의 테이블과 연관되게 만들어 주는 것
     private String title;
